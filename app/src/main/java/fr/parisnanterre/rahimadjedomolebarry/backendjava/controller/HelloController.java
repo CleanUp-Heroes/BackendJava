8442e7fc0ed59776a487f0791ecb63147aa90dd0
Nene Mariama Barry <barrymariam896@gmail.com>
2024-11-22 17:32:12 +0100

    add doc swagger

--- app/src/main/java/fr/parisnanterre/rahimadjedomolebarry/backendjava/controller/HelloController.java
+++ app/src/main/java/fr/parisnanterre/rahimadjedomolebarry/backendjava/controller/HelloController.java
@@ -4,11 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController; // les imports
 
-@RestController
+import fr.parisnanterre.models.HelloModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+
+@RestController
+@Tag(name = "Hello Controller")
 public class HelloController {
 
     @GetMapping("/Hello")
+
+    @Operation(
+            summary = "Returns a welcome message",
+            description = "This endpoint returns a simple message saying 'Hello from API'"
+    )
     
     public String getHello(){
 
---
+++ app/src/main/java/fr/parisnanterre/rahimadjedomolebarry/backendjava/controller/Operation.java
@@ -0,0 +1,5 @@
+package fr.parisnanterre.rahimadjedomolebarry.backendjava.controller;
+
+public @interface Operation {
+
+}
