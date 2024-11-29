51b01195df80ef08c7c63f4b2caa8585e98e0fac
Ibtissem-01 <ibtissem.rahim@outlook.fr>
2024-11-29 14:36:39 +0100

    patch sur HelloController.java

--- app/src/main/java/fr/parisnanterre/rahimadjedomolebarry/backendjava/controller/HelloController.java
+++ app/src/main/java/fr/parisnanterre/rahimadjedomolebarry/backendjava/controller/HelloController.java
@@ -1,10 +1,8 @@
 package fr.parisnanterre.rahimadjedomolebarry.backendjava.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RestController; // les imports
 
-import fr.parisnanterre.models.HelloModel;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -21,10 +19,8 @@ public class HelloController {
     )
     
     public String getHello(){
-
         return "Hello from API";
 
     }
 }
 
-// faire localhost:8080/hello  dans chrome par exemple => pour avoir  hello from API
